880abef373f34620f2502f4cf3b5ab0ae81b2dba
Megan Hainline <meg.hainline@gmail.com>
2018-09-29 20:23:12 -0500

    final

--- CG 11-1/Program.cs
+++ CG 11-1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace CG_11_1
 {
@@ -6,7 +8,31 @@ namespace CG_11_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader file = new StreamReader("C:\\Users\\megha\\source\\repos\\CG 11-1\\CG 11-1\\StudentData.txt");
+
+            List<Student> students = new List<Student>();
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(" ");
+                Student student = new Student(items[0]);
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    student.AddScore(Int32.Parse(items[i]));
+                }
+
+                students.Add(student);
+            }
+            
+            foreach (Student item in students)
+            {
+                if (item.SixOrGreater())
+                    Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ CG 11-1/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_11_1
+{
+    class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+        public bool SixOrGreater()
+        {
+            return scores.Count >= 7;
+        }
+
+
+
+
+    }
+
+
+}
